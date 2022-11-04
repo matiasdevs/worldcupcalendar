be86f4e74ab8e7bc6d3f4b50c2f6ba8cd42e2711
Matias Amariya <me.em@outlook.com>
2022-11-03 21:44:32 -0300

    arrumado icon da copa

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ function createGame(player1, hour, player2){
   `
 };
 
-let delay = -0.3;
+let delay = -0.1;
 function createCard(date, day, games) {
-  delay = delay + 0.3;
+  delay = delay + 0.1;
 return `   
       <div class="card" style="animation-delay: ${delay}s">
         <h2>${date}<span>${day}</span></h2>
